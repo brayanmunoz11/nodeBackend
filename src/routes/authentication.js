6837c6f9599d384f1e09bfd70a3c3832269630f0
Your Name <you@example.com>
2020-12-12 15:40:30 -0500

    validacion del sign in

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -15,6 +15,20 @@ router.get('/signup', (req, res) => {
     failureRedirect: '/signup',
     failureFlash: true
   }));
+
+  router.get('/signin', (req, res) => {
+    res.render('auth/signin');
+  });
+
+  router.post('/signin', (req, res, next) => {
+    
+    passport.authenticate('local.signin', {
+      successRedirect: '/profile',
+      failureRedirect: '/signin',
+      failureFlash: true
+    })(req, res, next);
+  });
+  
   
   router.get('/profile',(req, res) => {
     res.send(' this is your profile');
