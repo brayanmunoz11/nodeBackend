8f72f89aeefe32b9d362d688d06333d1214d2ba1
Your Name <you@example.com>
2020-12-12 10:09:51 -0500

    primer commit

---
+++ src/routes/authentication.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+
+// SIGNUP
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+  });
+
+
+  router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+  }));
+  
+  router.get('/profile',(req, res) => {
+    res.send(' this is your profile');
+  });
+
+
+module.exports = router;
