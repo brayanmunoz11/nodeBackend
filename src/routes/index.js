f2e7ab005dbe8890bd863691df2503466571590e
Your Name <you@example.com>
2020-12-12 10:23:47 -0500

    commit para despliegue a heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('hello world');
+    res.render('bienenido a mi API');
 });
 
 module.exports = router;
