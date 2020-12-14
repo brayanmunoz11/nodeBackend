4995111937c6df94e5b6a7c9d0c9a0789d9076ec
Your Name <you@example.com>
2020-12-13 23:40:33 -0500

    api signin terminado

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -31,19 +31,31 @@ router.get('/signup', (req, res) => {
   
   //SIGNIN
   router.post('/signin',upload.fields([]), async (req, res, next) => {
-
+    res.setHeader('Content-Type', 'application/json')
+		res.setHeader('Access-Control-Allow-Origin', '*');
     console.log(req.body)
-
+    var message = '';
+    var users = {};
     const user1 = req.body; 
     const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.users WHERE username = ?', [user1.nombre]);
     if (rows.length > 0) {
       const user = rows[0];
       const validPassword = await helpers.matchPassword(user1.password, user.password)
       if (validPassword) {
-        
+        users = user;
+        message = 'usario logeado';
         console.log(user);
-      } 
-    } 
+      } else{
+        
+        message = 'password incorrecto';
+      }
+    } else{
+      message = 'usuario no existe';
+    }
+    res.status(200).json({
+      data: users,
+      message: message
+    }); 
   });
   
   
