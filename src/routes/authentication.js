8223a13e769fd5b8bd5d8eb30dfa25e8f38d72c0
Jose Alvarez <jose12_08ro.ck@hotmail.com>
2020-12-14 22:29:04 -0500

    Commit Sign up

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -6,59 +6,72 @@ const pool = require('../database');
 const helpers = require('../lib/helpers');
 
 let multer = require('multer');
+const { request } = require('express');
 let upload = multer();
 
 
 
 // SIGNUP
 router.get('/signup', (req, res) => {
-    res.render('auth/signup');
-    
-  });
+  res.render('auth/signup');
+
+});
 
 
-  router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
-    failureFlash: true
-  }));
+router.post('/signup', upload.fields([]), async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  //const { fullname } = req.body;
+  console.log(req.body)
+  let newUser = {
+    fullname,
+    username,
+    password
+  };
+  newUser.password = await helpers.encryptPassword(password);
+  // Saving in the Database
+  const result = await pool.query('INSERT INTO heroku_ac61479f38e9e23.users SET ? ', newUser);
+  newUser.id = result.insertId;
+  return done(null, newUser);
 
-  router.get('/signin', (req, res) => {
-    res.render('auth/signin');
-  });
+});
 
+router.get('/signin', (req, res) => {
   
-  
-  //SIGNIN
-  router.post('/signin',upload.fields([]), async (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json')
-		res.setHeader('Access-Control-Allow-Origin', '*');
-    console.log(req.body)
-    var message = '';
-    var users = {};
-    const user1 = req.body; 
-    const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.users WHERE username = ?', [user1.nombre]);
-    if (rows.length > 0) {
-      const user = rows[0];
-      const validPassword = await helpers.matchPassword(user1.password, user.password)
-      if (validPassword) {
-        users = user;
-        message = 'usario logeado';
-        console.log(user);
-      } else{
-        
-        message = 'password incorrecto';
-      }
-    } else{
-      message = 'usuario no existe';
+});
+
+
+
+//SIGNIN
+router.post('/signin', upload.fields([]), async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  console.log(req.body)
+  var message = '';
+  var users = {};
+  const user1 = req.body;
+  const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.users WHERE username = ?', [user1.nombre]);
+  if (rows.length > 0) {
+    const user = rows[0];
+    const validPassword = await helpers.matchPassword(user1.password, user.password)
+    if (validPassword) {
+      users = user;
+      message = 'usario logeado';
+      console.log(user);
+    } else {
+
+      message = 'password incorrecto';
     }
-    res.status(200).json({
-      data: users,
-      message: message
-    }); 
+  } else {
+    message = 'usuario no existe';
+  }
+  res.status(200).json({
+    data: users,
+    message: message
   });
-  
-  
+});
+
+
 
 
 module.exports = router;
