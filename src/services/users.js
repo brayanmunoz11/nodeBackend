58e080c1a18613e10284ae7096c38632baa65293
Your Name <you@example.com>
2020-12-27 20:14:51 -0500

    id configurado

--- src/services/users.js
+++ src/services/users.js
@@ -8,14 +8,17 @@ class UserServices {
   async getUser(user) {
     var message = '';
     var users = {};
-    var valid = false;
+    
+    var valid = false;  
     
     const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.user WHERE usuario = ?', [user.nombre]);
     if (rows.length > 0) {
       const user1 = rows[0];
       const validPassword = await helpers.matchPassword(user.password, user1.password)
       if (validPassword) {
+        
         users = user1;
+        
         message = 'usario logeado';
         valid = true;
         console.log(user);
@@ -40,8 +43,9 @@ class UserServices {
     newUser.password = await helpers.encryptPassword(user.password);
     // Saving in the Database
     const result = await pool.query('INSERT INTO heroku_ac61479f38e9e23.user SET ? ', newUser);
-
-    return newUser
+    const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.user WHERE nombre = ?', [newUser.nombre]);
+    
+    return rows
   }
 }
 module.exports = UserServices;
