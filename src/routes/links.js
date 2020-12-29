3f75e00ef2f961d062158ee1561c60b68a6c5e31
Your Name <you@example.com>
2020-12-29 10:14:37 -0500

    listar por usuario

--- src/routes/links.js
+++ src/routes/links.js
@@ -40,18 +40,18 @@ router.post('/add', upload.fields([]), async (req, res, next) => {
 });
 
 router.post('/list',  async (req, res, next) => {
-    const {tipo} = req.body;
+    const {tipo, iduser} = req.body;
     console.log(req.body)
     try {
         let list
         if (tipo == 'css'){
-            list = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.css');
+            list = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.css WHERE iduser = ?', [iduser]);
         }
         else if (tipo == 'html'){
-            list = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.html');
+            list = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.html WHERE iduser2 = ?', [iduser]););
         }
         else if (tipo == 'js'){
-            list = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.js');
+            list = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.js WHERE iduser1 = ?', [iduser]););
         }
         res.status(201).json({
             data: list,
