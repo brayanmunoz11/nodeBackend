d9fdfeda22e461aa6c8e1816a98a2961416fa8ba
Your Name <you@example.com>
2021-01-02 22:19:55 -0500

    user edit

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -15,14 +15,14 @@ router.post('/signup', upload.fields([]), async (req, res, next) => {
       message: 'user created'
     });
   }
-  catch(err) {
+  catch (err) {
     next(err);
   }
 });
 
 //SIGNIN
 router.post('/signin', upload.fields([]), async (req, res, next) => {
-  try{
+  try {
     const [user, message, valid] = await userService.getUser(req.body);
     res.status(200).json({
       data: user,
@@ -30,31 +30,36 @@ router.post('/signin', upload.fields([]), async (req, res, next) => {
       valid: valid
     });
   }
-  catch(err) {
+  catch (err) {
     next(err);
   }
 });
 
 
-router.post('/useredit/:id', upload.fields([]), async (req, res) => {
-  const { id } = req.params;
-  const { nombre,apellido,usuario,email} = req.body; 
+router.post('/useredit/:id', upload.fields([]), async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { nombre, apellido, usuario, email } = req.body;
 
-  const newUser = {
-      nombre , 
-      apellido ,
-      usuario ,
-      email ,
+    const newUser = {
+      nombre,
+      apellido,
+      usuario,
+      email
 
-  };
+    };
 
-  await pool.query('UPDATE heroku_ac61479f38e9e23.user set ? WHERE id = ?', [newUser, id]);
-  const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.user WHERE id = ?', [id]);
-  res.status(200).json({
-    data: rows,
-    message: "user updated"
-  });
+    let vaaaal = await pool.query('UPDATE heroku_ac61479f38e9e23.user set ? WHERE id = ?', [newUser, id]);
+    const rows = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.user WHERE id = ?', [id]);
+    res.status(200).json({
+      data: rows,
+      message: "user updated"
+    });
 
+  }catch (err) {
+    next(err);
+  }
+  
 
 });
 
