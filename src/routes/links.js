39547756fc942b01d6151a3be92e66db80b451d4
Your Name <you@example.com>
2021-01-02 20:54:24 -0500

    editar v1

--- src/routes/links.js
+++ src/routes/links.js
@@ -70,26 +70,38 @@ router.get('/delete/:id', async (req, res) => {
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async (req, res) => {
+/*router.get('/edit/:id', async (req, res) => {
     const { id } = req.params;
     const links = await pool.query('SELECT * FROM heroku_ac61479f38e9e23.css WHERE id = ?', [id]);
     res.render('links/edit', {link: links[0]});
-});
+});*/
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id', async (req, res, next)=>{
     const { id } = req.params;
-    const { title, description, url} = req.body;
-    const newLink = {
-        title,
-        description,
-        url
+    const { nombre, cuerpo, tipo} = req.body;
+    const newArchive = {
+        nombre,
+        cuerpo,
     };
-    console.log(newLink);
-    await pool.query('UPDATE heroku_ac61479f38e9e23.css set ? WHERE id = ?', [newLink, id]);
-    req.flash('success', 'Link Updated Successfully');
-    res.redirect('/links');
-});
 
+    try {
+        
+        if (tipo == 'css'){
+            await pool.query('UPDATE heroku_ac61479f38e9e23.css set ? WHERE idcss = ?', [newArchive, id]);
+        }
+        else if (tipo == 'html'){
+            await pool.query('UPDATE heroku_ac61479f38e9e23.html set ? WHERE idhtml = ?', [newArchive, id]);
+        }
+        else if (tipo == 'js'){
+            await pool.query('UPDATE heroku_ac61479f38e9e23.js set ? WHERE idjs = ?', [newArchive, id]);
+        }
+        res.status(201).json({
+            message: 'file update'
+        });
+    }catch(err){
+        next(err);
+    }       
+});
 
 
 module.exports = router;
