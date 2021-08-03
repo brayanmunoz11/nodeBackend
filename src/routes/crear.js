a3306da353544ccf580777c9b8b40293fbd54fbc
Oscar Fernando <oscar.cabellos@unmsm.edu.pe>
2021-08-03 09:15:30 -0500

    mejora3

--- src/routes/crear.js
+++ src/routes/crear.js
@@ -3,15 +3,29 @@ const router = express.Router();
 
 const pool = require('../database');
 
+const helpers = require('../lib/helpers');
 let multer = require('multer');
 let upload = multer();
 
 router.post('/crearDoctor', upload.fields([]), async (req, res, next) => {
   try {
-    console.log(req.body)
-    const { nombre, apellido, dni, sexo, especialidad, turno, correo, contrasena } = req.body;
-
+    const { nombre, apellido, usuario, sexo, especialidad, turno, correo, contrasena } = req.body;
+    const passwordencriptado = await helpers.encryptPassword(contrasena);
+    const newDoctor = {
+      nombre,
+      apellido,
+      usuario,
+      sexo,
+      especialidad,
+      turno,
+      correo,
+      contrasena: passwordencriptado,
+      tipoUsuario: 'doctor'
+    }
+    await pool.query('CALL heroku_ac61479f38e9e23.crearDoctor(?)', [Object.values(newDoctor)]);
+    const doctores = await pool.query('CALL heroku_ac61479f38e9e23.listarDoctores()');
     res.status(201).json({
+      doctores: doctores[0],
       msg: 'Doctor creado'
     });
   } catch (err) {
@@ -20,10 +34,25 @@ router.post('/crearDoctor', upload.fields([]), async (req, res, next) => {
 });
 router.post('/crearPaciente', upload.fields([]), async (req, res, next) => {
   try {
-    console.log(req.body)
-    // const { nombre, apellido, dni, sexo, especialidad, turno, correo, contrasena } = req.body;
+    const { nombre, apellido, usuario, sexo, vigencia, tipoSeguro, correo, contrasena, centro } = req.body;
+    const passwordencriptado = await helpers.encryptPassword(contrasena);
+    const newPaciente = {
+      nombre,
+      apellido,
+      usuario,
+      sexo,
+      vigencia,
+      tipoSeguro,
+      correo,
+      contrasena: passwordencriptado,
+      tipoUsuario: 'paciente',
+      centro
+    }
+    await pool.query('CALL heroku_ac61479f38e9e23.crearPaciente(?)', [Object.values(newPaciente)]);
+    const pacientes = await pool.query('CALL heroku_ac61479f38e9e23.listarPacientes()');
 
     res.status(201).json({
+      paciente: pacientes[0],
       msg: 'Paciente creado'
     });
   } catch (err) {
@@ -33,9 +62,18 @@ router.post('/crearPaciente', upload.fields([]), async (req, res, next) => {
 router.post('/crearCama', upload.fields([]), async (req, res, next) => {
   try {
     console.log(req.body)
-    // const { nombre, apellido, dni, sexo, especialidad, turno, correo, contrasena } = req.body;
+    const { sala, ocupada, paciente } = req.body
+
+    const newCama = {
+      sala,
+      estado: ocupada,
+      idUsuario: paciente
+    }
+    await pool.query('CALL heroku_ac61479f38e9e23.crearCama(?)', [Object.values(newCama)]);
+    const camas = await pool.query('CALL heroku_ac61479f38e9e23.listarCamas()');
 
     res.status(201).json({
+      camas: camas[0],
       msg: 'Cama creado'
     });
   } catch (err) {
